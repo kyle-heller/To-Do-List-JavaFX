c0cf57012eea22b1c58d60363992149db5ed3976
kyle.heller <kyle.heller@gmail.com>
2024-02-19 21:23:18 -0500

    Imported javafx depencencies, created blank window in main

--- toDoList/src/main/java/TodoList.java
+++ toDoList/src/main/java/TodoList.java
@@ -1,11 +1,26 @@
-public class TodoList {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class TodoList extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Blank JavaFX Window");
+        primaryStage.setScene(new Scene(new StackPane(), 300, 250));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     public void addTask(String taskName) {
         System.out.println(taskName);
     }
 
     public boolean containsTask(String taskName) {
-       return true;
+        return true;
     }
 
 }
