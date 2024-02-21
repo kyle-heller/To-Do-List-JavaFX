4122fa19e894d212df1516f0a8f028dbafcc8d0f
kyle.heller <kyle.heller@gmail.com>
2024-02-21 18:04:37 -0500

    Added overloaded constructors

--- toDoList/src/main/java/Task.java
+++ toDoList/src/main/java/Task.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Task {
+import java.time.LocalDate;
+public class Task {
+    static int taskCount;
+    int taskID;
+    String taskName;
+    boolean completed, importantHighlight;
+    LocalDate dueDate;
+//    example: LocalDate dueDate = LocalDate.of(2024, 1, 31); // YYYY-MM-DD
+
+    public Task(String taskName) {
+        this(taskName, null);
+    }
+    public Task(String taskName, LocalDate dueDate) {
+        this(taskName, dueDate, false);
+    }
+    public Task(String taskName, LocalDate dueDate, boolean importantHighlight) {
+        this.taskName = taskName;
+        this.dueDate = dueDate;
+        this.importantHighlight = importantHighlight;
+        taskID = assignTaskID();
+    }
+
+    private static int assignTaskID() {
+        taskCount++;
+        return taskCount;
+    }
 }
