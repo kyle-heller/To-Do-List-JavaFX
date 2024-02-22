9a539a059b4aceacc58a7343bba41c639a45221b
kyle.heller <kyle.heller@gmail.com>
2024-02-21 21:24:21 -0500

    Refactored ToDoList class to work with javafx, window loads

--- toDoList/src/main/java/TodoList.java
+++ toDoList/src/main/java/TodoList.java
@@ -1,12 +1,18 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.cell.CheckBoxListCell;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 import java.util.ArrayList;
 
-public class TodoList { //extends Application
+public class TodoList extends Application {
 
-    ArrayList taskList = new ArrayList(10);
+    private ObservableList<Task> taskList = FXCollections.observableArrayList();
 
     public TodoList() {
 
@@ -16,12 +22,37 @@ public class TodoList { //extends Application
 //
 //    }
 
-//    @Override
-//    public void start(Stage primaryStage) {
-//        primaryStage.setTitle("Blank JavaFX Window");
-//        primaryStage.setScene(new Scene(new StackPane(), 300, 250));
-//        primaryStage.show();
-//    }
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("To-Do List");
+
+        // Create the ListView for tasks
+        ListView<Task> taskListView = new ListView<>(taskList);
+        taskListView.setCellFactory(lv -> new CheckBoxListCell<>(
+                task -> task.taskCompletedProperty(), // Corrected to use the taskCompleted property
+                new StringConverter<Task>() {
+                    @Override
+                    public String toString(Task task) {
+                        return task.getTaskName(); // modify this as needed
+                    }
+
+                    @Override
+                    public Task fromString(String string) {
+                        return null; // no conversion from string needed
+                    }
+                }
+        ));
+
+        // Main layout container
+        BorderPane mainLayout = new BorderPane();
+        // mainLayout.setTop(addTaskButton); // You need to create and set this button
+        mainLayout.setCenter(taskListView);
+
+        // Create the scene and show the stage
+        Scene scene = new Scene(mainLayout, 400, 600); // Set the window size
+        primaryStage.setScene(scene);
+        primaryStage.show(); // Show the stage
+    }
 
 
     public void addTask(String taskName) {
@@ -64,21 +95,21 @@ public class TodoList { //extends Application
 
 
     public static void main(String[] args) {
-        TodoList td1 = new TodoList();
+//        TodoList td1 = new TodoList();
+//
+//        td1.addTask("LALALA");
+//        td1.addTask("LALALA2");
+//
+//
+//        System.out.println(td1);
+//
+//        td1.removeTask("LALALA");
+//        System.out.println(td1);
+//        td1.completeTask("LALALA2");
+//        System.out.println(td1);
 
-        td1.addTask("LALALA");
-        td1.addTask("LALALA2");
 
-
-        System.out.println(td1);
-
-        td1.removeTask("LALALA");
-        System.out.println(td1);
-        td1.completeTask("LALALA2");
-        System.out.println(td1);
-
-
-//        launch(args);
+        launch(args);
     }
 
 
