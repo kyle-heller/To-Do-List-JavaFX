e6e189524a56ece62f4de17d9571b2f420adb3d2
kyle.heller <kyle.heller@gmail.com>
2024-02-21 20:48:03 -0500

    Changed completed bool to taskCompleted, added getters and setters

--- toDoList/src/main/java/Task.java
+++ toDoList/src/main/java/Task.java
@@ -3,7 +3,7 @@ public class Task {
     static int taskCount;
     int taskID;
     String taskName;
-    boolean completed, importantHighlight;
+    boolean taskCompleted, importantHighlight;
     LocalDate dueDate;
 //    example: LocalDate dueDate = LocalDate.of(2024, 1, 31); // YYYY-MM-DD
 
@@ -17,11 +17,25 @@ public class Task {
         this.taskName = taskName;
         this.dueDate = dueDate;
         this.importantHighlight = importantHighlight;
+        this.taskCompleted = false;
         taskID = assignTaskID();
     }
 
     private static int assignTaskID() {
         taskCount++;
         return taskCount;
+    }
+
+    public String getTaskName() { return this.taskName; }
+
+    public void setTaskName(String newName) { this.taskName = newName; }
+    public void setTaskCompleted() { this.setTaskCompleted(true); }
+    public void setTaskCompleted(boolean value) { this.taskCompleted = value; }
+
+
+
+    public String toString()
+    {
+        return taskID + " " + taskName + " " + taskCompleted;
     }
 }
